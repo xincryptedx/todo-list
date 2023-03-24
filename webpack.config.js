dc304cd0fe010df7d958f6172646f306b889572e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-24 18:55:22 -0400

    Configure webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDo List",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
