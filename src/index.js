42c4ca51a2b12e41459902f1e5070caaa84e1fee
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-25 19:22:37 -0400

    Add some calls to index.js to test once event method

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ function doOnEventOne() {
   console.log("Done because of event one!");
 }
 
+function doOnEventTwo() {
+  console.log("Done because of event two!");
+}
+
 Events.on("eventOne", doOnEventOne);
 Events.off("eventOne", doOnEventOne);
 Events.on("eventOne", doOnEventOne);
 Events.emit("eventOne");
+Events.emit("eventOne");
+
+Events.once("eventTwo", doOnEventTwo);
+Events.emit("eventTwo");
+Events.emit("eventTwo");
