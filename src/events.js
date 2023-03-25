7645ef2a1d912b556c89a37deb8b80e0d707bcaf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-25 18:56:06 -0400

    Return this for method chaining and add offAll method

--- src/events.js
+++ src/events.js
@@ -1,31 +1,34 @@
 const eventsManager = {
-  events: [],
+  events: {},
   emit(eventName, payload) {
     if (this.events[eventName]) {
-      this.events[eventName].forEach((fn) => {
-        fn(payload);
-      });
+      this.events[eventName].forEach((fn) => fn(payload));
     }
+    return this;
   },
   on(eventName, callbackFn) {
+    if (typeof callbackFn !== "function") {
+      throw new TypeError("Expected a function");
+    }
     if (!this.events[eventName]) {
       this.events[eventName] = [];
     }
     this.events[eventName].push(callbackFn);
-
-    console.log(`EVENT ON: ${this.events[eventName]}`);
+    return this;
   },
   off(eventName, callbackFn) {
     if (this.events[eventName]) {
       for (let i = 0; i < this.events[eventName].length; i += 1) {
         if (this.events[eventName][i] === callbackFn) {
-          console.log(`EVENT OFF:${this.events[eventName]}`);
-
           this.events[eventName].splice(i, 1);
           break;
         }
       }
     }
+    return this;
+  },
+  offAll() {
+    this.events = {};
   },
 };
 
