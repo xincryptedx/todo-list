150ecee3840da5d148f1d6ca5ddb06871866f003
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-25 19:08:53 -0400

    Refactor eventsManager to keep events private and update offAll to use delete

--- src/events.js
+++ src/events.js
@@ -1,35 +1,41 @@
-const eventsManager = {
-  events: {},
-  emit(eventName, payload) {
-    if (this.events[eventName]) {
-      this.events[eventName].forEach((fn) => fn(payload));
-    }
-    return this;
-  },
-  on(eventName, callbackFn) {
-    if (typeof callbackFn !== "function") {
-      throw new TypeError("Expected a function");
-    }
-    if (!this.events[eventName]) {
-      this.events[eventName] = [];
-    }
-    this.events[eventName].push(callbackFn);
-    return this;
-  },
-  off(eventName, callbackFn) {
-    if (this.events[eventName]) {
-      for (let i = 0; i < this.events[eventName].length; i += 1) {
-        if (this.events[eventName][i] === callbackFn) {
-          this.events[eventName].splice(i, 1);
-          break;
+const eventsManager = (() => {
+  const events = {};
+
+  return {
+    emit(eventName, payload) {
+      if (events[eventName]) {
+        events[eventName].forEach((fn) => fn(payload));
+      }
+      return this;
+    },
+    on(eventName, callbackFn) {
+      if (typeof callbackFn !== "function") {
+        throw new TypeError("Expected a function");
+      }
+      if (!events[eventName]) {
+        events[eventName] = [];
+      }
+      events[eventName].push(callbackFn);
+      return this;
+    },
+    off(eventName, callbackFn) {
+      if (events[eventName]) {
+        for (let i = 0; i < events[eventName].length; i += 1) {
+          if (events[eventName][i] === callbackFn) {
+            events[eventName].splice(i, 1);
+            break;
+          }
         }
       }
-    }
-    return this;
-  },
-  offAll() {
-    this.events = {};
-  },
-};
+      return this;
+    },
+    offAll() {
+      Object.keys(events).forEach((eventName) => {
+        delete events[eventName];
+      });
+      return this;
+    },
+  };
+})();
 
 export default eventsManager;
