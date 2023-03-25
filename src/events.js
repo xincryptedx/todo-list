147f327fbb57713f05e23498759a7248e7cc9017
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-25 17:14:12 -0400

    Create events module skeleton

---
+++ src/events.js
@@ -0,0 +1,13 @@
+const Events = {
+  on() {
+    console.log("Event On");
+  },
+  off() {
+    console.log("Event Off");
+  },
+  emit() {
+    console.log("Event Emitted");
+  },
+};
+
+export default Events;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./style.css";
+import Events from "./events";
+
+Events.on();
+Events.off();
+Events.emit();
