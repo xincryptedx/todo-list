c31b1682d9363f08b375fa6922ace027c9c646ed
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-25 20:08:21 -0400

    Add data object that will hold task data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,20 @@
 const dataManager = (() => {
-  // Data objects
-  // Get data
-  // Set data
-  // Emit data changed when data is changed
+  const data = {
+    projects: {
+      general: {},
+      trash: {},
+    },
+  };
+
+  // Create task
+  // Move task
+  // Remove task
+  // Create project
+  // Move project
+  // Remove project
+  // Get data (also emit event with the data)
+  // Set data (also emit event with the data)
+  // Sub to appropriate events for getting or setting data
 })();
 
 export default dataManager;
