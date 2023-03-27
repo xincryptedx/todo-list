44d60cc7c3692593a02cc55f7bad64b47c51edf8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-26 23:07:03 -0400

    Remove events testing

--- src/index.js
+++ src/index.js
@@ -2,23 +2,5 @@ import "./style.css";
 import Events from "./events";
 import DataManager from "./data";
 
-function doOnEventOne() {
-  console.log("Done because of event one!");
-}
-
-function doOnEventTwo() {
-  console.log("Done because of event two!");
-}
-
-Events.on("eventOne", doOnEventOne);
-Events.off("eventOne", doOnEventOne);
-Events.on("eventOne", doOnEventOne);
-Events.emit("eventOne");
-Events.emit("eventOne");
-
-Events.once("eventTwo", doOnEventTwo);
-Events.emit("eventTwo");
-Events.emit("eventTwo");
-
 window.dataManager = DataManager;
 Events.emit("init");
