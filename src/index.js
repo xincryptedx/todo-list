3dee7f61ce66d775dc5850cbb242eef6569c0b2d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-27 20:02:30 -0400

    Create dataSorter module skeleton and import it into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import Events from "./events";
 import DataManager from "./data";
+import DataSorter from "./dataSorter";
 
 window.dataManager = DataManager;
 Events.emit("init");
