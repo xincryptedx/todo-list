0cd07d347c4dd44975768e4b811bb064a2caaa05
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-04 14:10:49 -0400

    Import storage manager module and attatch it to the window for testing

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import DataManager from "./data";
 import DataFormatter from "./dataFormatter";
 import DomManager from "./domManager";
 import newElement from "./domElementBuilder";
+import StorageManager from "./storageManager";
 
 window.events = Events;
 window.dataManager = DataManager;
 window.dataFormatter = DataFormatter;
 window.domManager = DomManager;
+window.storageManager = StorageManager;
 
 window.thisBody = document.body;
 window.newElement = newElement;
