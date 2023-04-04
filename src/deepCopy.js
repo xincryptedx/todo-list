ca5f38772da6a89b0118df2157017de6693929ab
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-04 16:41:25 -0400

    Add module for making deep copies of objects

---
+++ src/deepCopy.js
@@ -0,0 +1,12 @@
+function deepCopy(obj) {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+  const copy = Array.isArray(obj) ? [] : {};
+  Object.keys(obj).forEach((key) => {
+    copy[key] = deepCopy(obj[key]);
+  });
+  return copy;
+}
+
+export default deepCopy;
