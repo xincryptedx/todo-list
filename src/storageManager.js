db5dd3cc0375fd595e9b0fc620fa82ea6f169b66
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-04 14:09:21 -0400

    Add storage manager module skeleton

---
+++ src/storageManager.js
@@ -0,0 +1,8 @@
+const storageManager = (() => {
+  // Take json and turn it into data object
+  // Take data object and turn it into json
+  // Save json to local storage
+  // Load json from local storage
+})();
+
+export default storageManager;
