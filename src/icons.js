8c61e678df3954893fd9b12e742537547b90413f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-07 17:30:54 -0400

    Simplify icon names

--- src/icons.js
+++ src/icons.js
@@ -1,15 +1,19 @@
-const newTaskBtn = 0;
+const newTask = 0;
 const menuBtn = 0;
-const subtasksBtn = 0;
-const taskDetailsBtn = 0;
-const dateBtn = 0;
-const closeBtn = 0;
+const menuOpened = 0;
+const subtasks = 0;
+const taskDetails = 0;
+const date = 0;
+const close = 0;
+const check = 0;
 
 export default {
-  newTaskBtn,
+  newTaskBtn: newTask,
   menuBtn,
-  subtasksBtn,
-  taskDetailsBtn,
-  dateBtn,
-  closeBtn,
+  menuOpened,
+  subtasksBtn: subtasks,
+  taskDetailsBtn: taskDetails,
+  dateBtn: date,
+  closeBtn: close,
+  check,
 };
