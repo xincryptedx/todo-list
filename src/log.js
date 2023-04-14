4faa5011a40d2cb74fbff451ad411ac4263ba469
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-14 04:12:49 -0400

    Write method for logging task restored message

--- src/log.js
+++ src/log.js
@@ -2,16 +2,24 @@ import Events from "./events";
 
 const log = (() => {
   let logText = "";
-  const messageTimeout = 3000;
+  const messageTimeout = 2000;
 
   // Set log message when task deleted
   const taskTrashed = () => {
-    logText = `Moved task to trash.`;
+    logText = "Moved task to trash.";
 
     Events.emit("log", { log: logText, timeout: messageTimeout });
   };
 
   Events.on("taskTrashed", taskTrashed);
+
+  const taskRestored = () => {
+    logText = "Task restored.";
+
+    Events.emit("log", { log: logText, timeout: messageTimeout });
+  };
+
+  Events.on("taskRestored", taskRestored);
 
   // Same but undeleted
 
